11e2df9a15657166b5453eb4e12b51611938e095
Black Box Research <drummerben@me.com>
2025-01-15 03:17:20 +0000

    fetched trade data

--- pages/accounts.py
+++ pages/accounts.py
@@ -2,6 +2,7 @@ import streamlit as st
 import time
 import nest_asyncio
 import asyncio
+import pandas as pd
 from utils.account_management import deploy_account
 from utils.database_management import execute_query
 from static.elements import tile, metric_tile
@@ -45,82 +46,106 @@ def summary_tiles(height, gain_value, win_rate_value, profit_factor_value, analy
                     tooltip=None
                     )
 
+@st.dialog("Add Account")
+def add_account_dialog(user_id):
+    # Check the number of active accounts for the current user
+    query = "SELECT COUNT(*) AS active_account_count FROM accounts WHERE user_id = %s AND active = TRUE"
+    result = execute_query(query, (user_id,))
+    
+    if result:
+        active_account_count = result[0]["active_account_count"]
+    else:
+        st.error("Could not retrieve account information. Please try again later.")
+        return
 
+    if active_account_count >= 3:
+        st.warning("You have reached the maximum limit of 3 connected accounts.")
+        return
 
+    # User input for adding a new account
+    account_name = st.text_input("Account Name", placeholder="e.g., Main Account")
+    login = st.text_input("Account Number", placeholder="Account Number")
+    password = st.text_input("Investor Password", placeholder="Investor Password", type="password")
+    server = st.text_input("Server", placeholder="Server")
+    platform = st.selectbox("Platform", ("mt4", "mt5"), placeholder="Platform")
 
+    confirm_add_account_button = st.button("Add Account", key="confirm_add_account_button", icon=":material/check:", type="secondary", use_container_width=True)
 
+    if confirm_add_account_button:
+        with st.spinner("Deploying account..."):
+            time.sleep(3)
+        with st.spinner("Fetching trade history..."):
+            try:
+                # Ensure there is an event loop in the current thread
+                try:
+                    loop = asyncio.get_event_loop()
+                except RuntimeError:
+                    # No event loop in the current thread; create a new one
+                    loop = asyncio.new_event_loop()
+                    asyncio.set_event_loop(loop)
+                
+                # Run the async function in the current thread's event loop
+                loop.run_until_complete(deploy_account(
+                    user_id=user_id,
+                    name=account_name,
+                    login=login,
+                    password=password,
+                    server_name=server,
+                    platform=platform,
+                ))
+                st.success("Account successfully added!")
+                time.sleep(2)
+                st.rerun()
+            except Exception as e:
+                st.error(f"Failed to add account: {e}")
+    
+def get_trades(account_id):
+    """
+    Retrieves all trades for a given account ID.
 
+    Args:
+        account_id (int): The ID of the account to retrieve trades for.
 
+    Returns:
+        list or None: A list of trades for the specified account ID, or None if an error occurs.
+    """
+    trades_query = "SELECT * FROM trades WHERE account_id = %s"
+    trades = execute_query(trades_query, (account_id,))
+    if trades is None:
+        print(f"Failed to retrieve trades for account_id: {account_id}")
+    return trades
 
+def get_balances(account_id):
+    """
+    Retrieves all balance operations for a given account ID.
 
+    Args:
+        account_id (int): The ID of the account to retrieve balance operations for.
+
+    Returns:
+        list or None: A list of balance operations for the specified account ID, or None if an error occurs.
+    """
+    balances_query = "SELECT * FROM balance_operations WHERE account_id = %s"
+    balances = execute_query(balances_query, (account_id,))
+    if balances is None:
+        print(f"Failed to retrieve balances for account_id: {account_id}")
+    return balances
 
 def accounts_page():
-    user_id = st.session_state["user_id"]
-
     st.subheader(f'My Accounts', anchor=False)
-
-    @st.dialog("Add Account")
-    def add_account_dialog():
-        # Check the number of active accounts for the current user
-        query = "SELECT COUNT(*) AS active_account_count FROM accounts WHERE user_id = %s AND active = TRUE"
-        result = execute_query(query, (user_id,))
-        
-        if result:
-            active_account_count = result[0]["active_account_count"]
-        else:
-            st.error("Could not retrieve account information. Please try again later.")
-            return
-
-        if active_account_count >= 3:
-            st.warning("You have reached the maximum limit of 3 connected accounts.")
-            return
-
-        # User input for adding a new account
-        account_name = st.text_input("Account Name", placeholder="e.g., Main Account")
-        login = st.text_input("Account Number", placeholder="Account Number")
-        password = st.text_input("Investor Password", placeholder="Investor Password", type="password")
-        server = st.text_input("Server", placeholder="Server")
-        platform = st.selectbox("Platform", ("mt4", "mt5"), placeholder="Platform")
-
-        confirm_add_account_button = st.button("Add Account", key="confirm_add_account_button", icon=":material/check:", type="secondary", use_container_width=True)
-
-        if confirm_add_account_button:
-            with st.spinner("Deploying account..."):
-                time.sleep(3)
-            with st.spinner("Fetching trade history..."):
-                try:
-                    # Ensure there is an event loop in the current thread
-                    try:
-                        loop = asyncio.get_event_loop()
-                    except RuntimeError:
-                        # No event loop in the current thread; create a new one
-                        loop = asyncio.new_event_loop()
-                        asyncio.set_event_loop(loop)
-                    
-                    # Run the async function in the current thread's event loop
-                    account = loop.run_until_complete(deploy_account(
-                        user_id=user_id,
-                        name=account_name,
-                        login=login,
-                        password=password,
-                        server_name=server,
-                        platform=platform,
-                    ))
-                    st.success("Account successfully added!")
-                    time.sleep(2)
-                    st.rerun()
-                except Exception as e:
-                    st.error(f"Failed to add account: {e}")
+    user_id = st.session_state["user_id"]
     
-    # Execute the query
+    # Execute the query to fetch user accounts
     query = "SELECT account_id, name, login FROM accounts WHERE user_id = %s AND active = TRUE"
     user_accounts = execute_query(query, (user_id,))
 
-    # Extract name and login values to display in the desired format
+    # Extract options and map them to account IDs
     if user_accounts:
-        account_options = [f"{account['name']} ({account['login']})" for account in user_accounts]  # Format as "name (login)"
+        account_map = {f"{account['name']} ({account['login']})": account['account_id'] for account in user_accounts}
+        account_options = list(account_map.keys())
         select_disabled = False
     else:
+        account_map = {}
         account_options = ["No accounts available"]
         select_disabled = True
 
@@ -128,10 +153,12 @@ def accounts_page():
 
     account_selection = select_account_column.selectbox("Select Account", account_options, disabled=select_disabled)
 
-    if add_account_column.button(label="Add Account", key="open_add_account_dialog", icon=":material/add_circle:", type="secondary", use_container_width=True):
-         add_account_dialog()
+    if add_account_column.button(label="Add Account", key="open_add_account_dialog", icon=":material/add_circle:", type="primary", use_container_width=True):
+         add_account_dialog(user_id)
 
     if account_selection != "No accounts available":
+        selected_account_id = account_map[account_selection]
+
         gain = 55
         win_rate = 64
         profit_factor = 1.32
@@ -143,6 +170,9 @@ def accounts_page():
             f"{profit_factor}",
             f"{analytiq_score}",
         )
+
+        trades = st.dataframe(pd.DataFrame(get_trades(selected_account_id)), hide_index=True, use_container_width=True)
+        balances = st.dataframe(pd.DataFrame(get_balances(selected_account_id)), hide_index=True, use_container_width=True)
 
 if __name__ == "__main__":
     accounts_page()
