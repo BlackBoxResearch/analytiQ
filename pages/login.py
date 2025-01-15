0e0415895668e5d5d4abe4b4cdd1eb963d913301
Black Box Research <drummerben@me.com>
2025-01-15 01:49:11 +0000

    testing dialog

--- pages/login.py
+++ pages/login.py
@@ -27,15 +27,40 @@ countries = [
     "Vanuatu", "Vatican City", "Venezuela", "Vietnam", "Yemen", "Zambia", "Zimbabwe"
 ]
 
-def login_page():
-
-    @st.dialog("Forgot Password")
-    def forgot_password():
+@st.dialog("Forgot Password")
+def forgot_password_dialog():
+    with st.form("forgot_password_form", border=False):
         recovery_email = st.text_input("Email")
-        if st.button("Submit", key="forgot_password_submit"):
+
+        if st.form_submit_button("Submit", key="forgot_password_submit"):
             st.info("Recovery email sent!")
 
+@st.dialog("Sign Up")
+def sign_up_dialog():
+    with st.form("sign_up_form", border=False):
+        col1, col2 = st.columns(2)
+        first_name = col1.text_input("First name")
+        last_name = col2.text_input("Last name")
+        email = col1.text_input("Email")
+        country = col2.selectbox("Country", countries)
+        password = col1.text_input("Password", type="password")
+        repeat_password = col2.text_input("Repeat password", type="password")
+        password_hint = st.text_input("Password hint")
+        marketing_preferences = st.checkbox("Opt out of communications")
+        terms_and_conditions = st.checkbox("I agree to the Terms and Conditions")
 
+        if st.form_submit_button("Proceed", use_container_width=True, type='secondary', icon=":material/app_registration:"):
+            if password != repeat_password:
+                st.error("Passwords do not match.")
+            elif not first_name or not last_name or not email or not password:
+                st.error("All fields are required.")
+            elif terms_and_conditions != True:
+                st.error("Please accept the terms and conditions to continue")
+            else:
+                register_user(first_name, last_name, email, country, password, password_hint, marketing_preferences)
+                st.success("Registration complete!", icon="🚀")
+
+def login_page():
     with st.container(border=False):
         col1, col2, col3 = st.columns([1,3,1], vertical_alignment="top")
         
@@ -57,10 +82,17 @@ def login_page():
 
                 email_input = st.text_input("Email")
                 password_input = st.text_input("Password", type="password")
-                col1, col2 = st.columns(2, vertical_alignment="top")
-                #col1.checkbox("Remember Me")
-                if col1.button("**Forgot Password?**", type="tertiary"):
-                    forgot_password()
+
+                forgot_password_button = st.button("forgot_password", use_container_width=True)
+
+                if forgot_password_button:
+                    forgot_password_dialog()
+
+                col11, col22 = st.columns(2, vertical_alignment="top")
+
+                if col11.button("**Forgot Password?**", type="tertiary"):
+                    forgot_password_dialog()
+
                 login_button = st.button(key="login_button", label="Log In", use_container_width=True, type='secondary', icon=":material/login:")
                 
                 if login_button:
@@ -92,34 +124,8 @@ def login_page():
                 unsafe_allow_html=True
             )
 
-            @st.dialog("Sign Up")
-            def sign_up():
-                with st.form("sign_up_form", border=False):
-                    col1, col2 = st.columns(2)
-                    first_name = col1.text_input("First name")
-                    last_name = col2.text_input("Last name")
-                    email = col1.text_input("Email")
-                    country = col2.selectbox("Country", countries)
-                    password = col1.text_input("Password", type="password")
-                    repeat_password = col2.text_input("Repeat password", type="password")
-                    password_hint = st.text_input("Password hint")
-                    marketing_preferences = st.checkbox("Opt out of communications")
-                    terms_and_conditions = st.checkbox("I agree to the Terms and Conditions")
-
-                    if st.form_submit_button("Proceed", use_container_width=True, type='secondary', icon=":material/app_registration:"):
-                        if password != repeat_password:
-                            st.error("Passwords do not match.")
-                        elif not first_name or not last_name or not email or not password:
-                            st.error("All fields are required.")
-                        elif terms_and_conditions != True:
-                            st.error("Please accept the terms and conditions to continue")
-                        else:
-                            register_user(first_name, last_name, email, country, password, password_hint, marketing_preferences)
-                            st.success("Registration complete!", icon="🚀")
-
-
             if st.button("Sign Up", icon=":material/app_registration:", use_container_width=True):
-                sign_up()
+                sign_up_dialog()
     
 if __name__ == "__main__":
     login_page()
