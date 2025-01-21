f0fe22e6ffab13b5f4a5b34dceaf57b52a10c6b1
Black Box Research <ben.hardman@blackboxresearch.co.uk>
2025-01-21 04:26:15 +0000

    unhide header

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,16 +12,15 @@ st.set_page_config(
         'About': "# This is a header. This is an *extremely* cool app!"
     })
 
-st.markdown(
-    """
-    <style>
-        /* Hide the Streamlit header */
-        header {visibility: hidden;}
-        manage-app-button {visibility: hidden;}
-    </style>
-    """,
-    unsafe_allow_html=True
-)
+# st.markdown(
+#     """
+#     <style>
+#         /* Hide the Streamlit header */
+#         header {visibility: hidden;}
+#     </style>
+#     """,
+#     unsafe_allow_html=True
+# )
 
 # Define pages with icons
 login_page = st.Page(page=login.login_page, title="Login", icon=":material/login:")
